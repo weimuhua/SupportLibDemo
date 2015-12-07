c55868091a3cb25c743609fd385397b39101d431
weimuhua <weimuhua0812@126.com>
2015-12-07 14:31:07 +0800

    "change : add DrawerLayoutActivity.java."

--- app/src/main/java/com/wayne/supportlibdemo/MainActivity.java
+++ app/src/main/java/com/wayne/supportlibdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package com.wayne.supportlibdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.wayne.supportlibdemo.v4.DrawerLayoutActivity;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private static final int INTENT_DRAWERLAYOUT_ACTIVITY = 0;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mListView = (ListView) findViewById(R.id.home_listview);
+    }
+
+    private void initData() {
+        String[] strArr = getResources().getStringArray(R.array.home_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strArr);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case INTENT_DRAWERLAYOUT_ACTIVITY:
+                startActivity(new Intent(this, DrawerLayoutActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/wayne/supportlibdemo/v4/DrawerLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.wayne.supportlibdemo.v4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wayne.supportlibdemo.R;
+
+public class DrawerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer_layout);
+    }
+}

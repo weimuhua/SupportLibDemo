14defa73cf203b7e592856684c722df47cd81cd9
weimuhua <weimuhua0812@126.com>
2015-12-09 11:15:11 +0800

    "change : complete test CardView."

--- app/src/main/java/com/wayne/supportlibdemo/v7/CardViewActivity.java
+++ app/src/main/java/com/wayne/supportlibdemo/v7/CardViewActivity.java
@@ -1,15 +1,65 @@
 package com.wayne.supportlibdemo.v7;
 
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.MenuItem;
+import android.widget.SeekBar;
 
 import com.wayne.supportlibdemo.R;
 
-public class CardViewActivity extends AppCompatActivity {
+public class CardViewActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private CardView mCardView;
+    private SeekBar mRadiusSeekBar;
+    private SeekBar mElevationSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+        initView();
+    }
+
+    private void initView() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        mCardView = (CardView) findViewById(R.id.cardview);
+        mRadiusSeekBar = (SeekBar) findViewById(R.id.cardview_radius_seekbar);
+        mElevationSeekBar = (SeekBar) findViewById(R.id.cardview_elevation_seekbar);
+        mRadiusSeekBar.setOnSeekBarChangeListener(this);
+        mElevationSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (mRadiusSeekBar == seekBar) {
+            mCardView.setRadius(progress);
+        } else if (mElevationSeekBar == seekBar) {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+                mCardView.setElevation(progress);
+            }
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 }
